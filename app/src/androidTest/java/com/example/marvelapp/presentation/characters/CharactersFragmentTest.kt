df5bc485f0b04168d7ded2bc9d6ef4de95bd5703
brunocn <brunnonicolai@gmail.com>
2023-08-07 08:10:05 -0300

    First Ui Testing

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -1,10 +1,20 @@
 package com.example.marvelapp.presentation.characters
 
+import android.view.View
+import com.example.marvelapp.R
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.marvelapp.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.Matcher
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -24,6 +34,22 @@ class CharactersFragmentTest {
     }
 
     @Test
-    fun shouldShowCharactersWhenViewIsCreated() {}
+    fun shouldShowCharactersWhenViewIsCreated() {
+//        onView(isRoot()).perform(waitFor(3000))
+        onView(
+            withId(R.id.rv_characters)
+        ).check(matches(isDisplayed()))
+    }
+    /*
+        private fun waitFor(delay: Long): ViewAction {
+            return object : ViewAction {
+                override fun getConstraints(): Matcher<View> = isRoot()
+                override fun getDescription(): String = "wait for $delay milliseconds"
+                override fun perform(uiController: UiController?, view: View?) {
+                    uiController?.loopMainThreadForAtLeast(delay)
+                }
+            }
+        }
+    */
 
 }
