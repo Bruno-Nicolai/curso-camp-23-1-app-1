4853e9c8f26ff4b4bd90993be1c43ff40af1da69
brunocn <brunnonicolai@gmail.com>
2023-08-07 09:33:45 -0300

    First Ui Testing Completely Working - Added A Json File to allow check test - MockWebServer

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -1,20 +1,20 @@
 package com.example.marvelapp.presentation.characters
 
-import android.view.View
 import com.example.marvelapp.R
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.marvelapp.extension.asJsonString
+import com.example.marvelapp.framework.di.BaseUrlModule
 import com.example.marvelapp.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import org.hamcrest.Matcher
+import dagger.hilt.android.testing.UninstallModules
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -22,34 +22,38 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@UninstallModules(BaseUrlModule::class)
 @HiltAndroidTest
 class CharactersFragmentTest {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    private lateinit var server: MockWebServer
+
     @Before
     fun setUp() {
+        server = MockWebServer().apply {
+            start(8080)
+        }
         launchFragmentInHiltContainer<CharactersFragment>()
     }
 
     @Test
     fun shouldShowCharactersWhenViewIsCreated() {
-//        onView(isRoot()).perform(waitFor(3000))
+        // FASTER/SMARTER
+        // doesn't depend on there is internet network
+        server.enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
+        // even before a request is made, my test already receives data which inflates the rv
+
         onView(
             withId(R.id.rv_characters)
         ).check(matches(isDisplayed()))
     }
-    /*
-        private fun waitFor(delay: Long): ViewAction {
-            return object : ViewAction {
-                override fun getConstraints(): Matcher<View> = isRoot()
-                override fun getDescription(): String = "wait for $delay milliseconds"
-                override fun perform(uiController: UiController?, view: View?) {
-                    uiController?.loopMainThreadForAtLeast(delay)
-                }
-            }
-        }
-    */
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
 
 }
